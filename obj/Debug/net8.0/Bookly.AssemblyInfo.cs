84110dd5c90c847dac72858136ec9f1b66c995eb
JONATAN NAIFELD <49549062@ort>
2025-10-16 09:23:16 -0300

    avance diseño

--- obj/Debug/net8.0/Bookly.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bookly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe1ec53c3dbcb12339a2ec4aad40d99776f96e55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8283cbbbaf7aea64f87ded714de0a22cd503920e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
