5f4cd17cf5676d3abef3c14428ba8057b690e9df
FACUNDO LUKACHER <49091985@ort>
2025-10-31 15:27:18 -0300

    cambios views

--- obj/Debug/net8.0/Bookly.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bookly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+004376a911a04c020afde68a88ef1b0f4f9c2017")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4260d05c67884c73b1864959fbb419c02523ee42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
