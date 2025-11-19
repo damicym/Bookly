9184cc31e28fe271247c9190041760f615dcdf62
JONATAN NAIFELD <49549062@ort>
2025-11-19 09:04:20 -0300

    panel detalles y solucion error detalles

--- obj/Debug/net8.0/Bookly.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bookly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d15dcbb691cc80fcdb591ff97daec0fef067adc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd062649801695cb1bb56844149b1af523d36ace")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
